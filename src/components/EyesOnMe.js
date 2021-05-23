1599c0e9ff67790b38366e1ee616719d7c822c9e
Leon Fan <leonkind@gmail.com>
2021-05-23 02:03:21 -0500

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+    logFocus = () => {
+        console.log('Good!')
+    }
+
+    logBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <div>
+                <button onFocus={this.logFocus} onBlur={this.logBlur}>button</button>
+            </div>
+        )
+    }
+}
